7a9e68dd46886541d81e378e29ea8bb12f0db481
EzekielMobile <bazzdude007@gmail.com>
2022-09-02 10:39:34 -0700

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,18 @@
  */
 int main(int argc, char *argv)
 {
+	int fd;
+
 	if (argc == 1 || argc > 2)
 	{
 		printf("USAGE: monty file");
+		return (EXIT_FAILURE);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: Can't oen file %s", argv[1]);
 		return (EXIT_FAILURE);
 	}
 }
